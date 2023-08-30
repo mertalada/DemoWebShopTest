ba3886b7c60b08bc12a746ae54e918a5ef3605e0
seda parça <sedaparca.93@gmail.com>
2023-08-30 22:10:24 +0300

    Seda PARÇA

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,67 @@
-public class Test {
-    public void DemoWebShop(){
+import Utility._Seda;
 
-
-
-
+public class Test extends _Seda {
+    @Override
+    public void Login() {
+        super.Login();
     }
+
+    @Override
+    public void Ürün_Listesinden_Ürün_Secimi() {
+        super.Ürün_Listesinden_Ürün_Secimi();
+    }
+
+    @Override
+    public void Sepet_Kontrolü() {
+
+        super.Sepet_Kontrolü();
+    }
+
+    @Override
+    public void Kargo_Bilgileri_Girisi() {
+        super.Kargo_Bilgileri_Girisi();
+    }
+
+    @Override
+    public void Adres_Bilgileri_İlk_Adresi_Tanımlama() {
+
+        super.Adres_Bilgileri_İlk_Adresi_Tanımlama();
+    }
+
+    @Override
+    public void AdAdres_Bilgileri_Kayıtlı_Adresten_Devam_Etme() {
+        super.AdAdres_Bilgileri_Kayıtlı_Adresten_Devam_Etme();
+    }
+
+    @Override
+    public void AdAdres_Bilgileri_Yeni_Adres_Kaydetme() {
+
+        super.AdAdres_Bilgileri_Yeni_Adres_Kaydetme();
+    }
+
+    @Override
+    public void Magaza_teslim_Secenegi_Secimi() {
+
+        super.Magaza_teslim_Secenegi_Secimi();
+    }
+
+    @Override
+    public void Ödeme_Yöntemi() {
+
+        super.Ödeme_Yöntemi();
+    }
+
+    @Override
+    public void Ödeme_Yöntemi_Dogrulama() {
+
+        super.Ödeme_Yöntemi_Dogrulama();
+    }
+
+    @Override
+    public void Siparis_Dogrulama() {
+
+        super.Siparis_Dogrulama();
+    }
+
+
 }
---
+++ src/Utility/_Seda.java
@@ -0,0 +1,284 @@
+package Utility;
+
+import Utility.BaseDriver;
+import Utility.MyFunction;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class _Seda extends BaseDriver{
+    Actions aksiyonlar = new Actions(driver);
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    @Test
+
+    public void Login() {
+        driver.get("https://demowebshop.tricentis.com/");
+
+
+        WebElement login = driver.findElement(By.xpath("//a[@class='ico-login']"));
+        login.click();
+
+        WebElement email = driver.findElement(By.xpath("//*[@id='Email']"));
+        email.sendKeys("sedaparca.93@gmail.com");
+        MyFunction.Wait(1);
+
+        WebElement password = driver.findElement(By.xpath("//*[@id='Password']"));
+        password.sendKeys("123456");
+        MyFunction.Wait(1);
+
+        WebElement loginbutton = driver.findElement(By.xpath("//*[@class='button-1 login-button']"));
+        loginbutton.click();
+        MyFunction.Wait(1);
+    }
+    @Test
+
+    public void Ürün_Listesinden_Ürün_Secimi() {
+
+        Login();
+
+        WebElement jewellery = driver.findElement(By.xpath("//*[@class='top-menu']/li[6]/a"));
+        jewellery.click();
+
+
+        new Actions(driver).scrollByAmount(0, 300).build().perform();
+        MyFunction.Wait(1);
+
+        //bileklikleri seçtin.
+        WebElement secim1tıkla = driver.findElement(By.xpath("//*[contains(@onclick,'catalog/71/1/1')] "));
+        secim1tıkla.click();
+
+        //scroll yaptın
+        new Actions(driver).scrollByAmount(0, 200).build().perform();
+        MyFunction.Wait(2);
+
+        WebElement materialmenu = driver.findElement(By.id("product_attribute_71_9_15"));
+        Select materialmenüolustur = new Select(materialmenu);
+        materialmenüolustur.selectByValue("46");
+        MyFunction.Wait(2);
+
+        WebElement length = driver.findElement(By.xpath("//*[@id='product_attribute_71_10_16']"));
+        length.sendKeys("25");
+
+        WebElement pendantsecim = driver.findElement(By.xpath("//*[@class='option-list']/li[2]/label"));
+        pendantsecim.click();
+        MyFunction.Wait(2);
+
+        WebElement quantity = driver.findElement(By.xpath("//*[@id='addtocart_71_EnteredQuantity']"));
+        aksiyonlar.moveToElement(quantity).click().sendKeys(Keys.BACK_SPACE).build().perform();
+        quantity.sendKeys("2");
+        MyFunction.Wait(2);
+
+        WebElement secim1 = driver.findElement(By.xpath("//*[@id='add-to-cart-button-71']"));
+        secim1.click();
+
+        MyFunction.Wait(2);
+
+
+        js.executeScript("window.scrollTo(0,0)");
+        MyFunction.Wait(2);
+    }
+    @Test
+    public void Sepet_Kontrolü() {
+
+        Ürün_Listesinden_Ürün_Secimi();
+
+        //sepete gidip ürünler eklenmiş mi,kontrol ediyorum.
+        WebElement shoppingcard = driver.findElement(By.xpath("//span[text()='Shopping cart']"));
+        shoppingcard.click();
+
+        MyFunction.Wait(2);
+
+    }
+    @Test
+    public void Kargo_Bilgileri_Girisi() {
+
+        Sepet_Kontrolü();
+        //sayfanın sonuna geldim.
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+        MyFunction.Wait(2);
+
+        WebElement countrybox = driver.findElement(By.xpath("//*[@id='CountryId']"));
+        aksiyonlar.moveToElement(countrybox).click().sendKeys("T").build().perform();
+        MyFunction.Wait(2);
+        new Actions(driver).scrollByAmount(0, 200).build().perform();
+        MyFunction.Wait(2);
+
+        Select country = new Select(countrybox);
+        country.selectByValue("77");
+        MyFunction.Wait(2);
+
+        WebElement agreebutton = driver.findElement(By.xpath("//*[@id='termsofservice']"));
+        agreebutton.click();
+        MyFunction.Wait(2);
+
+        WebElement checkout = driver.findElement(By.xpath("//*[@id='checkout']"));
+        checkout.click();
+        MyFunction.Wait(2);
+    }
+    @Test
+    public void Adres_Bilgileri_İlk_Adresi_Tanımlama(){
+
+        Kargo_Bilgileri_Girisi();
+
+        WebElement BillingCountryId =driver.findElement(By.xpath("//*[@id='BillingNewAddress_CountryId']"));
+        aksiyonlar.moveToElement(BillingCountryId).click().sendKeys("T").build().perform();
+
+        MyFunction.Wait(2);
+        new Actions(driver).scrollByAmount(0,200).build().perform();
+        MyFunction.Wait(2);
+
+        Select BillingCountry =new Select(BillingCountryId);
+        BillingCountry.selectByValue("77");
+        MyFunction.Wait(2);
+
+        WebElement BillingCity =driver.findElement(By.xpath("//*[@id='BillingNewAddress_City']"));
+        BillingCity.sendKeys("İstanbul");
+        MyFunction.Wait(2);
+
+        WebElement Adress1 =driver.findElement(By.xpath("//*[@id='BillingNewAddress_Address1']"));
+        Adress1.sendKeys("mahmutbey yolu 2.sokak");
+        MyFunction.Wait(2);
+
+        WebElement postacode =driver.findElement(By.xpath("//*[@id='BillingNewAddress_ZipPostalCode']"));
+        postacode.sendKeys("34000");
+        MyFunction.Wait(2);
+
+        WebElement phonenumber =driver.findElement(By.xpath("//*[@id='BillingNewAddress_PhoneNumber']"));
+        phonenumber.sendKeys("05350356773");
+        MyFunction.Wait(2);
+
+        WebElement continuee = driver.findElement(By.xpath("//*[@onclick='Billing.save()']"));
+        continuee.click();
+        MyFunction.Wait(2);
+
+        //burada bilgiler kaydedilsin mi diye bir alert çıkıyor,bunu nasıl kaydet butonuna tıklayabilirim.
+    }
+    @Test
+    public void AdAdres_Bilgileri_Kayıtlı_Adresten_Devam_Etme(){
+
+        Kargo_Bilgileri_Girisi();
+
+        WebElement adressselect = driver.findElement(By.xpath("//*[@id='billing-address-select']"));
+        adressselect.click();
+
+        Select newadress = new Select(adressselect);
+        newadress.selectByIndex(0);
+        MyFunction.Wait(2);
+
+        WebElement continuee = driver.findElement(By.xpath("//*[@onclick='Billing.save()']"));
+        continuee.click();
+        MyFunction.Wait(2);
+    }
+    @Test
+    public void AdAdres_Bilgileri_Yeni_Adres_Kaydetme(){
+
+        Kargo_Bilgileri_Girisi();
+
+        WebElement adressselect = driver.findElement(By.xpath("//*[@id='billing-address-select']"));
+        adressselect.click();
+
+        Select newadress = new Select(adressselect);
+        newadress.selectByVisibleText("New Address");
+        MyFunction.Wait(2);
+
+        WebElement BillingCountryId =driver.findElement(By.xpath("//*[@id='BillingNewAddress_CountryId']"));
+        aksiyonlar.moveToElement(BillingCountryId).click().sendKeys("T").build().perform();
+
+        MyFunction.Wait(2);
+        new Actions(driver).scrollByAmount(0,200).build().perform();
+        MyFunction.Wait(2);
+
+        Select BillingCountry =new Select(BillingCountryId);
+        BillingCountry.selectByValue("77");
+        MyFunction.Wait(2);
+
+        WebElement BillingCity =driver.findElement(By.xpath("//*[@id='BillingNewAddress_City']"));
+        BillingCity.sendKeys("İstanbul");
+        MyFunction.Wait(2);
+
+        WebElement Adress1 =driver.findElement(By.xpath("//*[@id='BillingNewAddress_Address1']"));
+        Adress1.sendKeys("mahmutbey yolu 2.sokak");
+        MyFunction.Wait(2);
+
+        WebElement postacode =driver.findElement(By.xpath("//*[@id='BillingNewAddress_ZipPostalCode']"));
+        postacode.sendKeys("34000");
+        MyFunction.Wait(2);
+
+        WebElement phonenumber =driver.findElement(By.xpath("//*[@id='BillingNewAddress_PhoneNumber']"));
+        phonenumber.sendKeys("05350356773");
+        MyFunction.Wait(2);
+
+        WebElement continuee = driver.findElement(By.xpath("//*[@onclick='Billing.save()']"));
+        continuee.click();
+        MyFunction.Wait(2);
+    }
+    @Test
+    public void Magaza_teslim_Secenegi_Secimi(){
+
+        AdAdres_Bilgileri_Kayıtlı_Adresten_Devam_Etme();
+
+        WebElement InStorepickUp = driver.findElement(By.xpath("//*[@id='PickUpInStore']"));
+        InStorepickUp.click();
+        MyFunction.Wait(2);
+
+        WebElement continue2 = driver.findElement(By.xpath("//*[@onclick='Shipping.save()']"));
+        continue2.click();
+        MyFunction.Wait(2);
+
+    }
+
+    @Test
+    public void Ödeme_Yöntemi(){
+
+        Magaza_teslim_Secenegi_Secimi();
+
+        WebElement paymentmethod = driver.findElement(By.xpath("//*[text()='Cash On Delivery (COD) (7.00)']"));
+        paymentmethod.click();
+        MyFunction.Wait(2);
+
+        new Actions(driver).scrollByAmount(0, 300).build().perform();
+        MyFunction.Wait(2);
+
+        WebElement continue3 = driver.findElement(By.xpath("//*[@onclick='PaymentMethod.save()']"));
+        continue3.click();
+        MyFunction.Wait(2);
+
+    }
+
+    @Test
+    public void Ödeme_Yöntemi_Dogrulama(){
+
+        Ödeme_Yöntemi();
+
+        new Actions(driver).scrollByAmount(0, 400).build().perform();
+        MyFunction.Wait(1);
+
+
+        WebElement contınue4 = driver.findElement(By.xpath("//*[@onclick='PaymentInfo.save()']"));
+        contınue4.click();
+        MyFunction.Wait(2);
+
+        //buraya kontrol eklenmeli.
+    }
+    @Test
+    public void Siparis_Dogrulama(){
+
+        Ödeme_Yöntemi_Dogrulama();
+
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+        MyFunction.Wait(2);
+
+        WebElement confirm = driver.findElement(By.xpath("//*[@onclick='ConfirmOrder.save()']"));
+        confirm.click();
+        MyFunction.Wait(2);
+
+        //kontrol eklenmeli.
+    }
+
+
+
+}
