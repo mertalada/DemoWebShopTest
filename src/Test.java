3c032c95f1a617ef1e2dfed2f1b128be481cf823
Mert Aladağ <heavyalad@gmail.com>
2023-09-01 16:58:09 +0300

    Negatif Kupon Hediye Kartı Kullanma-Lead

--- src/Test.java
+++ src/Test.java
@@ -65,8 +65,8 @@ public class Test {
         logIn.click();
 
         MyFunction.Wait(2);
-        email = driver.findElement(By.xpath("//input[@id='Email']"));
-        email.sendKeys("testingteam4@outlook.com");
+        WebElement email1 = driver.findElement(By.xpath("//input[@id='Email']"));
+        email1.sendKeys("testingteam4@outlook.com");
 
         MyFunction.Wait(2);
         WebElement pswrd= driver.findElement(By.cssSelector("#Password"));
@@ -181,6 +181,119 @@ public class Test {
 
         WebElement pdf=driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div.master-wrapper-main > div > div > div.page-title > a.button-2.pdf-order-button"));
         pdf.click();
+
+        MyFunction.Wait(1);
+
+        WebElement computers= driver.findElement(By.linkText("Computers"));
+        computers.click();
+
+        MyFunction.Wait(1);
+
+        WebElement notebooks= driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div.master-wrapper-main > div.center-2 > div.page.category-page > div.page-body > div.sub-category-grid > div:nth-child(2) > div > div > a > img"));
+        notebooks.click();
+
+        MyFunction.Wait(1);
+
+        WebElement laptop= driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div.master-wrapper-main > div.center-2 > div.page.category-page > div.page-body > div.product-grid > div > div > div.details > h2 > a"));
+        laptop.click();
+
+        WebElement addtocart2= driver.findElement(By.id("add-to-cart-button-31"));
+        addtocart2.click();
+
+        MyFunction.Wait(1);
+
+        WebElement shoppingcart2=driver.findElement(By.className("ico-cart"));
+        shoppingcart2.click();
+
+        MyFunction.Wait(1);
+
+        WebElement coupon = driver.findElement(By.name("discountcouponcode"));
+        coupon.sendKeys("kupondeneme");
+
+        WebElement applycoupon=driver.findElement(By.name("applydiscountcouponcode"));
+        applycoupon.click();
+
+        MyFunction.Wait(1);
+
+        WebElement giftcart= driver.findElement(By.name("giftcardcouponcode"));
+        giftcart.sendKeys("hediyekartıdeneme");
+
+        MyFunction.Wait(1);
+
+        WebElement addgiftcart= driver.findElement(By.name("applygiftcardcouponcode"));
+        addgiftcart.click();
+
+        MyFunction.Wait(1);
+
+        WebElement Country1 = driver.findElement(By.id("CountryId"));
+        Country1.click();
+
+        Select countryselect3 = new Select(Country1);
+        countryselect3.selectByValue("77");
+
+        WebElement postalcode3=driver.findElement(By.name("ZipPostalCode"));
+        postalcode3.sendKeys("01000");
+
+        WebElement agreebutton3= driver.findElement(By.id("termsofservice"));
+        agreebutton3.click();
+
+        MyFunction.Wait(1);
+
+        WebElement checkout3= driver.findElement(By.id("checkout"));
+        checkout3.click();
+
+        MyFunction.Wait(1);
+
+        WebElement adressselect1=driver.findElement(By.name("billing_address_id"));
+        adressselect1.sendKeys("New Address");
+
+        WebElement country4= driver.findElement(By.name("BillingNewAddress.CountryId"));
+        country4.click();
+
+        Select countryselect4 = new Select(country4);
+        countryselect4.selectByValue("77");
+
+        WebElement city3= driver.findElement(By.cssSelector("#BillingNewAddress_City"));
+        city3.sendKeys("Adana");
+
+        WebElement adress3= driver.findElement(By.id("BillingNewAddress_Address1"));
+        adress3.sendKeys("hürriyet mahallesi Adana/Turkey");
+
+        WebElement postalcode4= driver.findElement(By.id("BillingNewAddress_ZipPostalCode"));
+        postalcode4.sendKeys("01000");
+
+        WebElement phonenumber3= driver.findElement(By.id("BillingNewAddress_PhoneNumber"));
+        phonenumber3.sendKeys("05446675434");
+
+        MyFunction.Wait(1);
+
+        WebElement continuebutton1=driver.findElement(By.xpath("//input[@title='Continue']"));
+        continuebutton1.click();
+
+        MyFunction.Wait(1);
+
+        WebElement continuebutton21=driver.findElement(By.cssSelector("[onclick='Shipping.save()']"));
+        continuebutton21.click();
+
+        MyFunction.Wait(1);
+
+        WebElement continuebutton31=driver.findElement(By.cssSelector("[onclick='ShippingMethod.save()']"));
+        continuebutton31.click();
+
+        MyFunction.Wait(1);
+
+        WebElement continuebutton41=driver.findElement(By.cssSelector("[onclick='PaymentMethod.save()']"));
+        continuebutton41.click();
+
+        MyFunction.Wait(1);
+
+        WebElement continuebutton51=driver.findElement(By.cssSelector("[onclick='PaymentInfo.save()']"));
+        continuebutton51.click();
+
+        MyFunction.Wait(1);
+
+        WebElement confirmbutton3=driver.findElement(By.cssSelector("[onclick='ConfirmOrder.save()']"));
+        confirmbutton3.click();
 
 
 
